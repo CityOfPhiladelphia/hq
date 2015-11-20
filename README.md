5f6c49c4a34e1ac04eea428e96856fb30099f843
Gabriel Farrell <g@gsf.io>
2015-11-20 17:04:54 -0500

    Shorter namespace

--- README.md
+++ README.md
@@ -14,12 +14,12 @@ Management machine for the city.
 
 - AWS_ID=`access key ID for aws-cli`
 - AWS_SECRET=`secret access key for aws-cli`
-- PHILAGOV_MEDIA_BUCKET=`name of media bucket`
-- PHILAGOV_MEDIA_SYNC_BUCKET=`name of media bucket to sync to`
-- PHILAGOV_DB_HOST=`hostname of phila.gov database`
-- PHILAGOV_DB_PASS=`password for phila.gov database`
-- PHILAGOV_DB_BUCKET=`name of bucket for database dumps`
-- PHILAGOV_STAGING_DB_HOST=`hostname of phila.gov staging database`
+- PHILA_MEDIA_BUCKET=`name of media bucket`
+- PHILA_MEDIA_SYNC_BUCKET=`name of media bucket to sync to`
+- PHILA_DB_HOST=`hostname of phila.gov database`
+- PHILA_DB_PASS=`password for phila.gov database`
+- PHILA_DB_BUCKET=`name of bucket for database dumps`
+- PHILA_STAGING_DB_HOST=`hostname of phila.gov staging database`
 
 
 ## Variables for Travis CI
--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,9 @@ region = us-east-1
 EOF
 
 crontab - <<EOF
-20 3 * * * /usr/local/bin/aws s3 sync s3://$PHILAGOV_MEDIA_BUCKET s3://$PHILAGOV_MEDIA_SYNC_BUCKET
+20 3 * * * /usr/local/bin/aws s3 sync s3://$PHILA_MEDIA_BUCKET s3://$PHILA_MEDIA_SYNC_BUCKET
 
 # Use MYSQL_PWD instead of -p to avoid warning message
-20 4 * * * MYSQL_PWD=$PHILAGOV_DB_PASS mysqldump -C -h$PHILAGOV_DB_HOST -uwp wp | /usr/local/bin/aws s3 cp - s3://$PHILAGOV_DB_BUCKET/current.sql
-50 4 * * * /usr/local/bin/aws s3 cp s3://$PHILAGOV_DB_BUCKET/current.sql - | MYSQL_PWD=$PHILAGOV_DB_PASS mysql -h$PHILAGOV_STAGING_DB_HOST -uwp wp
+20 4 * * * MYSQL_PWD=$PHILA_DB_PASS mysqldump -C -h$PHILA_DB_HOST -uwp wp | /usr/local/bin/aws s3 cp - s3://$PHILA_DB_BUCKET/current.sql
+50 4 * * * /usr/local/bin/aws s3 cp s3://$PHILA_DB_BUCKET/current.sql - | MYSQL_PWD=$PHILA_DB_PASS mysql -h$PHILA_STAGING_DB_HOST -uwp wp
 EOF
