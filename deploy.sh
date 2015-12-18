c3258ed8b5c2daee705a74725d4e14acf5af48b7
Gabriel Farrell <g@gsf.io>
2015-12-18 17:00:27 -0500

    Also dump business db

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ crontab - <<EOF
 
 # Use MYSQL_PWD instead of -p to avoid warning message
 20 4 * * * MYSQL_PWD=$PHILA_DB_PASS mysqldump -C -h$PHILA_DB_HOST -uwp wp | /usr/local/bin/aws s3 cp - s3://$PHILA_DB_BUCKET/current.sql
+30 4 * * * MYSQL_PWD=$BUSINESS_DB_PASS mysqldump -C -h$BUSINESS_DB_HOST -uwp wp | /usr/local/bin/aws s3 cp - s3://$PHILA_DB_BUCKET/business.sql
 
 # Commented out 2015-11-23 because kdemi needs to keep changes on staging for a few days
 #50 4 * * * /usr/local/bin/aws s3 cp s3://$PHILA_DB_BUCKET/current.sql - | MYSQL_PWD=$PHILA_DB_PASS mysql -h$PHILA_STAGING_DB_HOST -uwp wp
