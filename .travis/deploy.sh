ccd8fc297faeb38f193320b398c9f195c0701c14
Gabriel Farrell <g@gsf.io>
2016-01-27 15:46:19 -0500

    Merge travis prep into deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+echo 'Setting up SSH access'
+openssl aes-256-cbc -K $encrypted_e99dd148f934_key -iv $encrypted_e99dd148f934_iv -in .travis/philagov2.pem.enc -out ~/.ssh/philagov2.pem -d
+chmod 400 ~/.ssh/philagov2.pem
+cat >> ~/.ssh/config <<EOF
+Host target
+  User ubuntu
+  HostName $IP
+  IdentityFile ~/.ssh/philagov2.pem
+EOF
+ssh-keyscan $IP >> ~/.ssh/known_hosts 2> /dev/null
+
 echo 'Syncing to target machine'
 rsync -rzp --delete --exclude=".*" --delete-excluded ./ target:hq
 
