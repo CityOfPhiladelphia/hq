e06d56f9a956b4bd9e12541f2534fc0d25ca907c
Gabriel Farrell <g@gsf.io>
2016-01-27 16:57:34 -0500

    Query for instance ID and joia in travis deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,19 +2,41 @@
 
 set -e
 
+echo 'Installing AWS CLI'
+cd
+wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/aws
+export PATH=~/bin:$PATH
+cd -
+
+echo 'Install Joia'
+curl https://raw.githubusercontent.com/CityOfPhiladelphia/joia/275ab6d7ee47e937c00c864e8fae061dd35d2d70/joia > ~/bin/joia
+chmod 755 ~/bin/joia
+
+echo 'Configuring AWS CLI'
+mkdir -p ~/.aws
+cat > ~/.aws/config <<EOF
+[default]
+aws_access_key_id = $AWS_ID
+aws_secret_access_key = $AWS_SECRET
+region = us-east-1
+EOF
+
+echo 'Retrieving instance ID from AWS'
+export INSTANCE_ID=$(aws ec2 describe-instances --filters
+  "Name=tag:Branch,Values=$TRAVIS_BRANCH"
+  "Name=tag:Project,Values=hq"
+  --query "Reservations[0].Instances[0].InstanceId" | tr -d '"')
+if [ ! "$INSTANCE_ID" ]; then
+  echo "No machine found for branch \"$TRAVIS_BRANCH\". Skipping deploy" 
+  exit 0
+fi
+
 echo 'Setting up SSH access'
 openssl aes-256-cbc -K $encrypted_e99dd148f934_key -iv $encrypted_e99dd148f934_iv -in .travis/philagov2.pem.enc -out ~/.ssh/philagov2.pem -d
 chmod 400 ~/.ssh/philagov2.pem
-cat >> ~/.ssh/config <<EOF
-Host target
-  User ubuntu
-  HostName $IP
-  IdentityFile ~/.ssh/philagov2.pem
-EOF
-ssh-keyscan $IP >> ~/.ssh/known_hosts 2> /dev/null
 
-echo 'Syncing to target machine'
-rsync -rzp --delete --exclude=".*" --delete-excluded ./ target:hq
-
-echo 'Executing deploy in app directory on target'
-ssh target 'cd hq && ./deploy.sh'
+joia host
+joia push > /dev/null
+joia deploy
