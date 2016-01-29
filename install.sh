b3b6b4d4013e06f597a09ac85d7bd8dd29f1786f
Gabriel Farrell <g@gsf.io>
2016-01-29 16:11:11 -0500

    Instance env handled by joia

--- install.sh
+++ install.sh
@@ -10,10 +10,3 @@ wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 cd -
-
-echo 'Copying .env to ~/.ssh/environment'
-cp .env ~/.ssh/environment
-
-echo 'Setting env vars for ssh deploys'
-echo 'PermitUserEnvironment yes' | sudo tee -a /etc/ssh/sshd_config > /dev/null
-sudo service ssh restart
