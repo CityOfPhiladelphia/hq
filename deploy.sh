4b06693d8475f0c99ee735247a392e15838399ee
Gabriel Farrell <g@gsf.io>
2016-02-01 13:50:18 -0500

    Now configuring aws in install

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-echo 'Configuring AWS CLI'
-mkdir -p ~/.aws
-cat > ~/.aws/config <<EOF
-[default]
-aws_access_key_id = $AWS_ID
-aws_secret_access_key = $AWS_SECRET
-region = us-east-1
-EOF
-
 echo 'Setting crontab'
 crontab - <<EOF
 20 3 * * * /usr/local/bin/aws s3 sync s3://$PHILA_MEDIA_BUCKET s3://$PHILA_MEDIA_SYNC_BUCKET
