1dde92a67e87a261ef8cfed8be3f6dc3905dcbf4
Gabriel Farrell <g@gsf.io>
2016-02-01 13:49:32 -0500

    Configure aws and get private key on install

--- install.sh
+++ install.sh
@@ -10,3 +10,16 @@ wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 cd -
+
+echo 'Configuring AWS CLI'
+mkdir -p ~/.aws
+cat > ~/.aws/config <<EOF
+[default]
+aws_access_key_id = $AWS_ID
+aws_secret_access_key = $AWS_SECRET
+region = us-east-1
+EOF
+
+echo 'Getting key for accessing other machines'
+aws s3 cp s3://phila-deploy/hq/id_rsa ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
