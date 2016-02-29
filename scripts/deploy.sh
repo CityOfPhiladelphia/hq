5d83ac63f14f20c206a90cdd5a95c7b0f4771e4b
Gabriel Farrell <g@gsf.io>
2016-02-29 12:43:53 -0500

    Add microsites dump to s3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,7 @@ PATH=$HOME/app/scripts:$PATH
 # Use MYSQL_PWD instead of -p to avoid warning message
 20 4 * * * MYSQL_PWD=$PHILA_DB_PASS mysqldump -C -h$PHILA_DB_HOST -uwp wp | /usr/local/bin/aws s3 cp - s3://$PHILA_DB_BUCKET/current.sql
 30 4 * * * MYSQL_PWD=$BUSINESS_DB_PASS mysqldump -C -h$BUSINESS_DB_HOST -uwp wp | /usr/local/bin/aws s3 cp - s3://$PHILA_DB_BUCKET/business.sql
+40 4 * * * MYSQL_PWD=$MICROSITES_DB_PASS mysqldump -C -h$MICROSITES_DB_HOST -uwp wp | /usr/local/bin/aws s3 cp - s3://$PHILA_DEPLOY_BUCKET/microsites/current.sql
 
 50 4 * * * /usr/local/bin/aws s3 cp s3://$PHILA_DB_BUCKET/current.sql - | MYSQL_PWD=$PHILA_DB_PASS mysql -h$PHILA_STAGING_DB_HOST -uwp wp
 
